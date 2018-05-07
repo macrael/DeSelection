c6bf26e9afd0e1350f8d45754fa504eaea777f37
MacRae Linton <55759+macrael@users.noreply.github.com>
2018-05-07 12:12:14 -0700

    Make it work with cmd-click

--- DeSelection.py
+++ DeSelection.py
@@ -4,21 +4,24 @@ import sublime
 import sublime_plugin
 
 class DeleteSelectionCommand(sublime_plugin.TextCommand):
-    def run(self, edit, event):
+    def run(self, edit, **kwargs):
+        event = kwargs['event']
         cursor_vector = (event['x'], event['y'])
         cursor_point = self.view.window_to_text(cursor_vector)
 
         cursor_region = sublime.Region(cursor_point, cursor_point)
         regions_to_remove = []
+
+        didSubtract = False
         for selected_region in self.view.selection:
             if selected_region.contains(cursor_region):
-                regions_to_remove.append(selected_region)
+                didSubtract = True
+                self.view.selection.subtract(selected_region)
 
-        if len(self.view.selection) == len(regions_to_remove):
-            self.view.selection.clear()
-        else:
-            for region in regions_to_remove:
-                self.view.selection.subtract(region)
+        if not didSubtract:
+            kwargs['additive'] = True
+            self.view.run_command("drag_select", kwargs)
+
 
     def want_event(self):
         return True
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 DeSelection
 =====
 
-DeSelection is a Sublime Text plugin that allows you to remove a piece of selection by option clicking on it.
+DeSelection is a Sublime Text plugin that allows you to remove a piece of selection by [super] clicking on it.
 
-This is especially useful for removing unwanted selections made by doing multiple selection.
+This is especially useful for removing unwanted selections made by during multiple selection.
+
+check this directory out (or a symlink to it) into `~/Library/Application\ Support/Sublime\ Text\ 3/Packages/` and ST should pick it up automatically.
